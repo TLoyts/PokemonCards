575e0e0f4313c4919cd41d1fdf894b327218b4e3
TLoyts <115872767+TLoyts@users.noreply.github.com>
2022-11-30 13:21:06 +0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -55,22 +55,22 @@
 
 <main>
 	<header>
-		<h1 id="⚓-top">Pokemon Cards <sup>V2</sup></h1>
+		<h1 id="⚓-top">Thẻ Pokemon <sup>V1</sup></h1>
 
-		<p class="author">By <a href="https://twitter.com/simeydotme"><svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter</title><path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/></svg> @simeydotme</a> |
-			<em><a href="https://github.com/simeydotme/pokemon-cards-css"><svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg> Simon Goellner</em>
+		<p class="author">By <a href="https://facebook.com/MilasS2T"><svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Facebook</title><path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/></svg> @Tloyts</a> |
+			<em><a href="https://github.com/S2TwKen"><svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg> Tloyts</em>
 		</p>
 
 		<section class="intro" id="⚓-intro">
 			<p>
-				A collection of <mark>advanced CSS</mark> styles to create
-				<mark>realistic-looking effects</mark> for the faces of Pokemon cards.
-			</p>
-			<br>
-			<p>
-				The cards use <mark>3d transforms</mark>, <mark>filters</mark>, <mark>blend modes</mark>,
-				<mark>css gradients</mark> and interactions to provide a unique experience when taking a closer look!
-			</p>
+				Một bộ sưu tập các kiểu <mark>CSS nâng cao</mark> để tạo
+<mark>hiệu ứng giống như thật</mark> cho mặt thẻ Pokemon.
+</p>
+<br>
+<p>
+Thẻ sử dụng <mark>biến đổi 3d</mark>, <mark>bộ lọc</mark>, <mark>chế độ hòa trộn</mark>,
+<mark>độ dốc css</mark> và tương tác để cung cấp trải nghiệm độc đáo khi xem xét kỹ hơn!
+</p>
 		</section>
 
 		<div class="showcase">
@@ -346,10 +346,10 @@
 		</a>
 	</h2>
 	<p>
-		The gradient effect of Pokemon VMax is more subtle, using a <mark
-			>larger background gradient</mark
+		Hiệu ứng chuyển màu của Pokemon VMax tinh tế hơn, sử dụng một <mark
+			>độ dốc nền lớn hơn</mark
 		>
-		which moves more slowly. But the <mark>texture effect is more pronounced</mark>.
+		nào chuyển động chậm hơn. Nhưng <mark>hiệu ứng kết cấu rõ rệt hơn</mark>.
 	</p>
 
 	<CardList>
@@ -375,14 +375,14 @@
 		</a>
 	</h2>
 	<p>
-		There's some VMax cards which show a <mark>full, or alternate artwork</mark>. These are
-		<mark>classed as "rainbow rare"</mark>
-		and have a similar effect to the rainbow cards. It's a really
-		<mark>vibrant and glittery overlay</mark>.
+		Có một số thẻ VMax hiển thị <mark>tác phẩm nghệ thuật đầy đủ hoặc thay thế</mark>. đó là
+<mark>được phân loại là "cầu vồng hiếm"</mark>
+và có tác dụng tương tự như thẻ cầu vồng. Đó là một thực sự
+<mark>lớp phủ rực rỡ và lấp lánh</mark>.
 	</p>
 	<p>
-		This is achieved with a <mark>background image of glitter/sparkles</mark>, and a texture pattern
-		background image, sandwiching the usual linear gradients.
+		Điều này đạt được với <mark>hình nền lấp lánh/lấp lánh</mark> và mẫu họa tiết
+hình nền, kẹp các gradient tuyến tính thông thường.
 	</p>
 
 	<CardList>
@@ -408,10 +408,10 @@
 		</a>
 	</h2>
 	<p>
-		Again back to the <mark>diagonal gradients overlaying a texture</mark>, VStar are quite
-		<mark>similar to teh Ultra Rare</mark>
-		(Full/Alt) cards. The cards are generally <mark>brighter with a pastel hue</mark>, though, which
-		makes the gradient and texture more subtle.
+		Quay trở lại với <mark>độ dốc đường chéo phủ lên kết cấu</mark>, VStar khá
+<mark>tương tự như Cực hiếm</mark>
+(Đầy đủ/Alt) thẻ. Tuy nhiên, các thẻ thường <mark>sáng hơn với màu nhạt</mark>
+làm cho độ dốc và kết cấu tinh tế hơn.
 	</p>
 
 	<CardList>
@@ -437,10 +437,10 @@
 		</a>
 	</h2>
 	<p>
-		Again back to the <mark>diagonal gradients overlaying a texture</mark>, VStar are quite
-		<mark>similar to teh Ultra Rare</mark>
-		(Full/Alt) cards. The cards are generally <mark>brighter with a pastel hue</mark>, though, which
-		makes the gradient and texture more subtle.
+		Quay trở lại với <mark>độ dốc đường chéo phủ lên kết cấu</mark>, VStar khá
+<mark>tương tự như Cực hiếm</mark>
+(Đầy đủ/Alt) thẻ. Tuy nhiên, các thẻ thường <mark>sáng hơn với màu nhạt</mark>
+làm cho độ dốc và kết cấu tinh tế hơn.
 	</p>
 
 	<CardList>
@@ -466,9 +466,9 @@
 		</a>
 	</h2>
 	<p>
-		The Rainbow Rare is a <mark>super glittery effect on top of pastel gradients</mark>. This is
-		achieved with a background image of glitter/sparkles and a
-		<mark>color-burn/hard-light background blend</mark> for some pastel gradients.
+		Rainbow Rare là một <mark>hiệu ứng siêu lấp lánh trên nền chuyển màu nhạt</mark>. Đây là
+đạt được với hình nền lấp lánh/lấp lánh và
+<mark>hỗn hợp nền cháy màu/ánh sáng mạnh</mark> cho một số chuyển màu phấn.
 	</p>
 
 	<CardList>
